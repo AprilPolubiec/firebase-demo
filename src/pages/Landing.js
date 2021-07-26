7f361d294958f6796692ec8f555b98954f5fdd88
April Polubiec <aprilpolubiec@gmail.com>
2021-07-26 15:29:33 -0500

    add sign in & sign up dummy buttons

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import Button from "@material-ui/core/Button"
+
 export default function Landing() {
     return (
-        <div>
-            Landing
+        <div id="landing">
+            <div className="auth">
+                <Button variant="contained" color="primary">
+                    Sign In
+                </Button>
+                <Button variant="outlined" color="primary">
+                    Sign Up
+                </Button>
+            </div>
         </div>
     )
 }
