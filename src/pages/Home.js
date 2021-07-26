725f7d309d2c49531100c553791bcaaf30e70607
April Polubiec <aprilpolubiec@gmail.com>
2021-07-26 16:17:51 -0500

    can sign out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import Button from "@material-ui/core/Button"
+
+import { auth } from "../firebase"
+
 export default function Home() {
-    return <>Home</>
+    const handleSignOut = () => {
+        return auth.signOut();
+    }
+    return <>
+        <Button variant="contained" color="primary" onClick={handleSignOut}>Sign Out</Button>
+    </>
 }
