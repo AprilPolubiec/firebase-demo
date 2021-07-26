e9d19101435ce36dbc790ed4647d2e576b785ade
April Polubiec <aprilpolubiec@gmail.com>
2021-07-26 14:07:03 -0500

    feat: add firebase intialization

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBiBjRcFn4kfUOvnu4TJwTktLefhhRJi7Y",
+    authDomain: "aprilpdev-fb-demo.firebaseapp.com",
+    projectId: "aprilpdev-fb-demo",
+    storageBucket: "aprilpdev-fb-demo.appspot.com",
+    messagingSenderId: "385835000670",
+    appId: "1:385835000670:web:6e142113cd2815674fa5cf",
+    measurementId: "G-GM1SJGF0EJ"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
