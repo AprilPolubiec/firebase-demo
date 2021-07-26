498045bdc9ca28cb0589f015c82c833291dd648c
April Polubiec <aprilpolubiec@gmail.com>
2021-07-26 15:16:14 -0500

    feat: create useAuth hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState, useEffect } from 'react'
+
+import { auth } from "../firebase"
+
+const AuthContext = React.createContext()
+
+const Auth = ({ children }) => {
+    const [user, setUser] = useState(auth.currentUser);
+    const [authLoaded, setAuthLoaded] = useState(false);
+
+    useEffect(() => {
+        const authListener = auth.onAuthStateChanged((user) => {
+            setUser(user)
+            setAuthLoaded(true)
+
+        })
+        return authListener()
+    }, [])
+
+    const context = {
+        user,
+        authLoaded
+    }
+
+    return (
+        <AuthContext.Provider value={context}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+//Auth wrapper component
+export default Auth;
+
+// useAuth hook
+export const useAuth = () => {
+    const auth = useContext(AuthContext)
+    if (!auth) {
+        throw new Error('You must call useAuth() inside of a <Auth />.')
+    }
+    return auth;
+}
