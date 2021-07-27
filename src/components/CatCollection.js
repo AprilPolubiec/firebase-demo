4383cfacbe63921c6053f0e82d3458fb3b0c9847
April Polubiec <aprilpolubiec@gmail.com>
2021-07-27 17:52:18 -0500

    keep track of cat created time & render all kitties

--- src/components/CatCollection.js
+++ src/components/CatCollection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -7,13 +7,36 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-import { db } from "../firebase";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import ImageListItemBar from "@material-ui/core/ImageListItemBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+
+import firebase, { db } from "../firebase";
 import { useAuth } from "../hooks/useAuth";
 
 export default function CatCollection() {
   const auth = useAuth();
   const [open, setOpen] = useState(false);
   const [name, setName] = useState("");
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const unsubscribeCats = db
+      .collection("users")
+      .doc(auth.user.uid)
+      .collection("cats")
+      .orderBy("createdAt")
+      .onSnapshot((snap) => {
+        const cat_data = snap.docs
+          .filter((d) => d.data().photo)
+          .map((d) => d.data());
+        setCats(cat_data);
+      });
+    return () => unsubscribeCats();
+  }, []);
 
   const handleClose = () => {
     setOpen(false);
@@ -26,7 +49,7 @@ export default function CatCollection() {
       .collection("users")
       .doc(auth.user.uid)
       .collection("cats")
-      .add({ name })
+      .add({ name, createdAt: firebase.firestore.FieldValue.serverTimestamp() })
       .then(handleClose)
       .catch((e) => console.error(e));
   };
@@ -36,6 +59,7 @@ export default function CatCollection() {
       <Button variant="outlined" color="primary" onClick={() => setOpen(true)}>
         Add Cat
       </Button>
+
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle id="form-dialog-title">Give your cat a name!</DialogTitle>
         <DialogContent>
@@ -58,6 +82,25 @@ export default function CatCollection() {
           </Button>
         </DialogActions>
       </Dialog>
+
+      <ImageList rowHeight={180} style={{ width: 500, height: 450 }}>
+        <ImageListItem key="Subheader" cols={2} style={{ height: "auto" }}>
+          <ListSubheader component="div">My Cats</ListSubheader>
+        </ImageListItem>
+        {cats.map((cat) => (
+          <ImageListItem key={cat.photo}>
+            <img src={cat.photo} alt={cat.name} />
+            <ImageListItemBar
+              title={cat.name}
+              actionIcon={
+                <IconButton aria-label={`info about ${cat.name}`}>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 }
