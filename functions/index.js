d80e2e6cb77666b34f5f37bd01fd55a76e9cef8b
April Polubiec <aprilpolubiec@gmail.com>
2021-07-27 17:51:56 -0500

    init app in functions & add cat photo on new cat doc

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,33 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const axios = require("axios").default;
-
-let firestore = admin.firestore();
 // This is a secure key - the dotfile exists in cloud but is not in git
 const { catApiKey } = require("./.config.json");
+
+admin.initializeApp();
+let firestore = admin.firestore();
 
 const CAT_ENDPOINT = "https://api.thecatapi.com/v1";
 
 exports.addUserToFirestore = functions.auth.user().onCreate((user) => {
-  firestore
-    .collection("users")
-    .doc(user.uid)
-    .set({
-      email: user.email,
-      emailVerified: user.emailVerified,
-      name: user.displayName,
-    });
+  firestore.collection("users").doc(user.uid).set({
+    email: user.email,
+    emailVerified: user.emailVerified,
+    name: user.displayName,
+  });
 });
 
-exports.getCatPhoto = functions.https.onCall((data, context) => {
-  console.log("Received request: ", data);
-  return axios
-    .get(`${CAT_ENDPOINT}/images/search?api_key=${catApiKey}`)
-    .then((res) => {
-      const photo = res[0].url;
-      return photo;
-    })
-    .catch((error) => error);
-});
+exports.getCatPhoto = functions.firestore
+  .document("users/{userId}/cats/{catId}")
+  .onCreate((snap, context) => {
+    return axios
+      .get(`${CAT_ENDPOINT}/images/search?api_key=${catApiKey}`)
+      .then((res) => {
+        const photo = res.data[0].url;
+        return snap.ref.set({ photo }, { merge: true });
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  });
