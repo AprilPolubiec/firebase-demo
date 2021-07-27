c07e153685fecceaaef879f8f1436dc97da5ed7e
April Polubiec <aprilpolubiec@gmail.com>
2021-07-27 17:11:37 -0500

    create user doc in firestore on sign up

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,31 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const axios = require("axios").default;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+let firestore = admin.firestore();
+// This is a secure key - the dotfile exists in cloud but is not in git
+const { catApiKey } = require("./.config.json");
+
+const CAT_ENDPOINT = "https://api.thecatapi.com/v1";
+
+exports.addUserToFirestore = functions.auth.user().onCreate((user) => {
+  firestore
+    .collection("users")
+    .doc(user.uid)
+    .set({
+      email: user.email,
+      emailVerified: user.emailVerified,
+      name: user.displayName,
+    });
+});
+
+// exports.getCatPhoto = functions.https.onCall((data, context) => {
+//   console.log("Received request: ", data);
+//   return axios
+//     .get(`${CAT_ENDPOINT}/images/search?api_key=${catApiKey}`)
+//     .then((res) => {
+//       const photo = res[0].url;
+//       return photo;
+//     })
+//     .catch((error) => error);
 // });
